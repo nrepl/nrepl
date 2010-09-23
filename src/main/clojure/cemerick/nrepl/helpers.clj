7f7ddd3df2d70a4a2df3c669f70cfb4af1b16b97
Chas Emerick <cemerick@snowtide.com>
2010-09-23 12:50:13 -0400

    slight reorg of helpers/tooling namespaces

--- src/main/clojure/cemerick/nrepl/helpers.clj
+++ src/main/clojure/cemerick/nrepl/helpers.clj
@@ -1,5 +1,5 @@
 (ns cemerick.nrepl.helpers
-  (:import (java.io StringReader)))
+  (:import (java.io File StringReader)))
 
 (defn escape
   [#^String s]
@@ -8,12 +8,3 @@
 (defn string-argument
   [s]
   (str \" (escape s) \"))
-
-(defn load-with-debug-info
-  "Load a code string using a source file path and name source-path for debug info
-   (line numbers, etc)."
-  [code file-path filename]
-  (clojure.lang.Compiler/load
-    (StringReader. code)
-    file-path
-    filename))
--- src/test/clojure/cemerick/nrepl/helpers_test.clj
+++ src/test/clojure/cemerick/nrepl/helpers_test.clj
@@ -1,10 +1,7 @@
 (ns cemerick.nrepl.helpers-test
-  (:use [cemerick.nrepl-test :only (def-repl-test repl-server-fixture)]
-    clojure.test)
+  (:use clojure.test)
   (:require
     [cemerick.nrepl.helpers :as helpers]))
-
-(use-fixtures :once repl-server-fixture)
 
 (deftest escape-and-string-argument
   (are [string escaped] (= escaped (helpers/escape string))
@@ -14,13 +11,3 @@
     "a" "\"a\""
     "\"a" "\"\\\"a\""))
 
-(def-repl-test load-code-with-debug-info
-  (repl "\n\n\n(defn function [])")
-  (is (= {:file "NO_SOURCE_PATH" :line 4}
-        (repl-value "(-> #'function meta (select-keys [:file :line]))")))
-  (repl (apply format
-          "(cemerick.nrepl.helpers/load-with-debug-info %s %s %s)"
-          (map helpers/string-argument
-            ["\n\n\n\n\n\n\n\n\n(defn dfunction [])" "path/from/source/root.clj" "root.clj"])))
-  (is (= {:file "path/from/source/root.clj" :line 10}
-        (repl-value "(-> #'dfunction meta (select-keys [:file :line]))"))))
