4476c58f93cd5a7600d45c539e492a2785bd0c80
Chas Emerick <cemerick@snowtide.com>
2010-10-04 13:25:50 -0400

    add optional color output to nrepl command line support

--- src/main/clojure/cemerick/nrepl/cmdline.clj
+++ src/main/clojure/cemerick/nrepl/cmdline.clj
@@ -1,24 +1,51 @@
 (ns cemerick.nrepl.cmdline
   (:require [cemerick.nrepl :as repl]))
 
-(defn- run-repl
-  [port]
-  (let [connection (repl/connect "localhost" port)
-        {:keys [major minor incremental qualifier]} *clojure-version*]
-    (println "network-repl")
-    (print (str "Clojure " major \. minor \. incremental))
-    (if (seq qualifier)
-      (println (str \- qualifier))
-      (println))
-    (loop [ns "user"]
-      (print (str ns "=> "))
-      (flush)
-      ((:send connection) (pr-str (read)))
-      (let [{:keys [out ns value]} ((:receive connection))]
-        (when (seq out) (print out))
-        (recur ns)))))
+(defn- ensure-newline
+  [s]
+  (if (= "\n" (last s))
+    s
+    (str s \newline)))
 
-(def #^{:private true} unary-options #{"--repl" "--server"})
+(def colored-output
+  {:err #(binding [*out* *err*]
+           (print "\033[31m")
+           (print %)
+           (print "\033[m")
+           (flush))
+   :out print
+   :value (fn [x]
+            (print "\033[34m")
+            (print x)
+            (print "\033[m")
+            (flush))})
+
+(defn- run-repl
+  ([port] (run-repl port nil))
+  ([port {:keys [prompt err out value]
+          :or {prompt #(print (str % "=> "))
+               err print
+               out print
+               value print}}]
+    (let [connection (repl/connect "localhost" port)
+          {:keys [major minor incremental qualifier]} *clojure-version*]
+      (println "network-repl")
+      (print (str "Clojure " major \. minor \. incremental))
+      (if (seq qualifier)
+        (println (str \- qualifier))
+        (println))
+      (loop [ns "user"]
+        (prompt ns)
+        (flush)
+        (recur (last
+                 (for [{:keys [ns] :as res} (repl/response-seq ((:send connection) (pr-str (read))))]
+                   (do
+                     (when (:value res) (value (:value res)))
+                     (when (:out res) (out (:out res)))
+                     (when (:err res) (err (:err res)))
+                     ns))))))))
+
+(def #^{:private true} unary-options #{"--repl" "--server" "--color"})
 
 (defn- split-args
   [args]
@@ -35,7 +62,6 @@
 (defn main
   [& args]
   (let [[options args] (split-args args)
-        _ (println options)
         [ssocket _] (repl/start-server (Integer/parseInt (or (options "--port") "0")))]
     (when-let [ack-port (options "--ack")]
       (binding [*out* *err*]
@@ -43,6 +69,6 @@
                    (.getLocalPort ssocket) ack-port)
           (:status (#'cemerick.nrepl/send-ack (.getLocalPort ssocket) (Integer/parseInt ack-port))))))
     (if (options "--repl")
-      (run-repl (.getLocalPort ssocket))
+      (run-repl (.getLocalPort ssocket) (when (options "--color") colored-output))
       ; need to hold process open with a non-daemon thread -- this should end up being super-temporary
       (Thread/sleep Long/MAX_VALUE))))
