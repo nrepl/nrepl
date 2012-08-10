db449ee8cc899db310b25ba59e93e884ddcd3ee7
Chas Emerick <chas@cemerick.com>
2012-08-10 07:31:38 -0400

    re-enable load-file tests

--- src/test/clojure/clojure/tools/nrepl/helpers_test.clj
+++ src/test/clojure/clojure/tools/nrepl/helpers_test.clj
@@ -1,5 +1,4 @@
-(ns #^{:doc ""
-       :author "Chas Emerick"}
+(ns ^{:author "Chas Emerick"}
   clojure.tools.nrepl.helpers-test
   (:import (java.io File))
   (:use [clojure.tools.nrepl-test :only (def-repl-test repl-server-fixture)]
@@ -8,37 +7,36 @@
     [clojure.tools.nrepl :as nrepl]
     [clojure.tools.nrepl.helpers :as helpers]))
 
-(comment ;TODO
 (def project-base-dir (File. (System/getProperty "nrepl.basedir" ".")))
 
 (use-fixtures :once repl-server-fixture)
 
 (def-repl-test load-code-with-debug-info
-  (repl-receive "\n\n\n(defn function [])")
-  (is (= {:file "NO_SOURCE_PATH" :line 4}
-        (repl-value "(-> #'function meta (select-keys [:file :line]))")))
+  (repl-eval client "\n\n\n(defn function [])")
+  (is (= [{:file "NO_SOURCE_PATH" :line 4}]
+         (repl-values client "(-> #'function meta (select-keys [:file :line]))")))
   
-  (repl-receive (helpers/load-file-command
-                  "\n\n\n\n\n\n\n\n\n(defn dfunction [])"
-                  "path/from/source/root.clj"
-                  "root.clj"))
+  (repl-eval client
+             (helpers/load-file-command
+               "\n\n\n\n\n\n\n\n\n(defn dfunction [])"
+               "path/from/source/root.clj"
+               "root.clj"))
   
   (is (= [{:file "path/from/source/root.clj" :line 10}]
-        (nrepl/values-with connection
-          (-> #'dfunction
-            meta
-            (select-keys [:file :line]))))))
+        (repl-values client
+          (nrepl/code
+            (-> #'dfunction
+              meta
+              (select-keys [:file :line])))))))
 
 (def-repl-test load-file-with-debug-info
-  (repl-receive (helpers/load-file-command
-                  (File. project-base-dir "load-file-test/clojure/tools/nrepl/load_file_sample.clj")
-                  (File. project-base-dir "load-file-test")))
-  (repl-receive (helpers/load-file-command
-                  (.getAbsolutePath (File. project-base-dir "load-file-test/clojure/tools/nrepl/load_file_sample.clj"))
-                  (File. project-base-dir "load-file-test")))
+  (repl-eval client
+             (helpers/load-file-command
+               (File. project-base-dir "load-file-test/clojure/tools/nrepl/load_file_sample.clj")
+               (File. project-base-dir "load-file-test")))
   (is (= [{:file "clojure/tools/nrepl/load_file_sample.clj" :line 5}]
-        (nrepl/values-with connection
-          (-> #'clojure.tools.nrepl.load-file-sample/dfunction
-            meta
-            (select-keys [:file :line]))))))
-)
+         (repl-values client
+           (nrepl/code 
+             (-> #'clojure.tools.nrepl.load-file-sample/dfunction
+               meta
+               (select-keys [:file :line])))))))
