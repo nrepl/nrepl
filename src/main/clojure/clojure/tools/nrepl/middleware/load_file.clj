74070be7ca333722fa4ef727cdfb629a22c34aa2
Chas Emerick <chas@cemerick.com>
2012-08-15 07:22:49 -0400

    tweak doc

--- src/main/clojure/clojure/tools/nrepl/middleware/load_file.clj
+++ src/main/clojure/clojure/tools/nrepl/middleware/load_file.clj
@@ -5,9 +5,9 @@
 
 (defn ^{:dynamic true} load-file-code
   "Given the contents of a file, its _source-path-relative_ path,
-   and its filename, returns an expression that, when evaluated,
-   will load those contents with appropriate filename references
-   and line numbers in metadata, etc."
+   and its filename, returns a string of code (or seq of expressions)
+   that, when evaluated, will load those contents with appropriate
+   filename references and line numbers in metadata, etc."
   [file file-path file-name]
   (apply format
     "(clojure.lang.Compiler/load (java.io.StringReader. %s) %s %s)"
