cf0bb7e7e8d7fe650110c0c2fd8d92b9bf8a91a2
Chas Emerick <chas@cemerick.com>
2012-10-04 01:29:09 -0400

    now that messages on the same transport are no longer serialized, tweak tests so that messages that *need* to be serialized are all put on the same session

--- src/test/clojure/clojure/tools/nrepl/load_file_test.clj
+++ src/test/clojure/clojure/tools/nrepl/load_file_test.clj
@@ -10,36 +10,38 @@
 (use-fixtures :once repl-server-fixture)
 
 (def-repl-test load-code-with-debug-info
-  (nrepl/message timeout-client {:op "load-file" :file "\n\n\n(defn function [])"})
+  (doall (nrepl/message timeout-session
+                        {:op "load-file" :file "\n\n\n(defn function [])"}))
   (is (contains?
         ; different versions of Clojure use different default :file metadata
         #{[{:file "NO_SOURCE_PATH" :line 4}]
           [{:file "NO_SOURCE_FILE" :line 4}]}
-        (repl-values timeout-client
+        (repl-values timeout-session
           (nrepl/code
             (-> #'function
               meta
               (select-keys [:file :line]))))))
   
-  (nrepl/message timeout-client {:op "load-file"
-                                 :file "\n\n\n\n\n\n\n\n\n(defn afunction [])"
-                                 :file-path "path/from/source/root.clj"
-                                 :file-name "root.clj"})
+  (doall (nrepl/message timeout-session {:op "load-file"
+                                         :file "\n\n\n\n\n\n\n\n\n(defn afunction [])"
+                                         :file-path "path/from/source/root.clj"
+                                         :file-name "root.clj"}))
   (is (= [{:file "path/from/source/root.clj" :line 10}]
-         (repl-values timeout-client
+         (repl-values timeout-session
            (nrepl/code
              (-> #'afunction
                meta
                (select-keys [:file :line])))))))
 
 (def-repl-test load-file-with-debug-info
-  (nrepl/message timeout-client
-                 {:op "load-file"
-                  :file (slurp (File. project-base-dir "load-file-test/clojure/tools/nrepl/load_file_sample.clj"))
-                  :file-path "clojure/tools/nrepl/load_file_sample.clj"
-                  :file-name "load_file_sample.clj"})
+  (doall
+    (nrepl/message timeout-session
+                   {:op "load-file"
+                    :file (slurp (File. project-base-dir "load-file-test/clojure/tools/nrepl/load_file_sample.clj"))
+                    :file-path "clojure/tools/nrepl/load_file_sample.clj"
+                    :file-name "load_file_sample.clj"}))
   (is (= [{:file "clojure/tools/nrepl/load_file_sample.clj" :line 5}]
-         (repl-values timeout-client
+         (repl-values timeout-session
            (nrepl/code 
              (-> #'clojure.tools.nrepl.load-file-sample/dfunction
                meta
