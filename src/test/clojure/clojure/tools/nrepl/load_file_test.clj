78c2c91a945c6499a7fa78b4465f195c6f78f778
Александар Симић <asimic@gmail.com>
2018-05-12 08:13:01 +0100

    Eastwood warning fix
    Added # in front of doall.
    
    Added additional ';' in front of a comment so that it's indented
    nicely.
    
    And then the rest are just indentation issues.
    
    Eastwood warning:
    
    src/test/clojure/clojure/tools/nrepl/load_file_test.clj:12:3:
    unused-ret-vals: Should use return value of function call, but it is
    discarded: (doall (nrepl/message timeout-session {:op "load-file",
    :file "\n\n\n(defn function [])"}))
    
    src/test/clojure/clojure/tools/nrepl/load_file_test.clj:24:3:
    unused-ret-vals: Should use return value of function call, but it is
    discarded: (doall (nrepl/message timeout-session {:op "load-file",
    :file "\n\n\n\n\n\n\n\n\n(defn afunction [])", :file-path
    "path/from/source/root.clj", :file-name "root.clj"}))
    
    src/test/clojure/clojure/tools/nrepl/load_file_test.clj:36:3:
    unused-ret-vals: Should use return value of function call, but it is
    discarded: (doall (nrepl/message timeout-session {:op "load-file",
    :file (slurp (File. project-base-dir
    "load-file-test/clojure/tools/nrepl/load_file_sample.clj")),
    :file-path "clojure/tools/nrepl/load_file_sample.clj", :file-name
    "load_file_sample.clj"}))
    
    src/test/clojure/clojure/tools/nrepl/load_file_test.clj:52:3:
    unused-ret-vals: Should use return value of function call, but it is
    discarded: (doall (nrepl/message session {:op "load-file", :file "(def
    a (+ 1 (+ 2 (+ 3 (+ 4 (+ 5 6))))))\n (def b 2) (def c 3) (def
    ^{:internal true} d 4)", :file-path "path/from/source/root.clj",
    :file-name "root.clj"}))

--- src/test/clojure/clojure/tools/nrepl/load_file_test.clj
+++ src/test/clojure/clojure/tools/nrepl/load_file_test.clj
@@ -9,22 +9,21 @@
 (use-fixtures :each repl-server-fixture)
 
 (def-repl-test load-code-with-debug-info
-  (doall (nrepl/message timeout-session
-                        {:op "load-file" :file "\n\n\n(defn function [])"}))
+  #(doall (nrepl/message timeout-session
+                         {:op "load-file" :file "\n\n\n(defn function [])"}))
   (is (contains?
-        ; different versions of Clojure use different default :file metadata
-        #{[{:file "NO_SOURCE_PATH" :line 4}]
-          [{:file "NO_SOURCE_FILE" :line 4}]}
-        (repl-values timeout-session
-          (nrepl/code
-            (-> #'function
-              meta
-              (select-keys [:file :line]))))))
-  
-  (doall (nrepl/message timeout-session {:op "load-file"
-                                         :file "\n\n\n\n\n\n\n\n\n(defn afunction [])"
-                                         :file-path "path/from/source/root.clj"
-                                         :file-name "root.clj"}))
+       ;; different versions of Clojure use different default :file metadata
+       #{[{:file "NO_SOURCE_PATH" :line 4}]
+         [{:file "NO_SOURCE_FILE" :line 4}]}
+       (repl-values timeout-session
+                    (nrepl/code
+                     (-> #'function
+                         meta
+                         (select-keys [:file :line]))))))
+  #(doall (nrepl/message timeout-session {:op "load-file"
+                                          :file "\n\n\n\n\n\n\n\n\n(defn afunction [])"
+                                          :file-path "path/from/source/root.clj"
+                                          :file-name "root.clj"}))
   (is (= [{:file "path/from/source/root.clj" :line 10}]
          (repl-values timeout-session
            (nrepl/code
@@ -33,7 +32,7 @@
                (select-keys [:file :line])))))))
 
 (def-repl-test load-file-with-debug-info
-  (doall
+  #(doall
     (nrepl/message timeout-session
                    {:op "load-file"
                     :file (slurp (File. project-base-dir "load-file-test/clojure/tools/nrepl/load_file_sample.clj"))
@@ -41,27 +40,26 @@
                     :file-name "load_file_sample.clj"}))
   (is (= [{:file "clojure/tools/nrepl/load_file_sample.clj" :line 5}]
          (repl-values timeout-session
-           (nrepl/code 
-             (-> #'clojure.tools.nrepl.load-file-sample/dfunction
-               meta
-               (select-keys [:file :line])))))))
+                      (nrepl/code
+                       (-> #'clojure.tools.nrepl.load-file-sample/dfunction
+                           meta
+                           (select-keys [:file :line])))))))
 
 (def-repl-test load-file-with-print-vars
   (set! *print-length* 3)
   (set! *print-level* 3)
-  (doall
+  #(doall
     (nrepl/message session {:op "load-file"
                             :file "(def a (+ 1 (+ 2 (+ 3 (+ 4 (+ 5 6))))))
-                                  (def b 2) (def c 3) (def ^{:internal true} d 4)"
+                                   (def b 2) (def c 3) (def ^{:internal true} d 4)"
                             :file-path "path/from/source/root.clj"
                             :file-name "root.clj"}))
-
   (is (= [4]
          (repl-values session (nrepl/code d)))))
 
 (def-repl-test load-file-response-no-ns
   (is (not (contains? (nrepl/combine-responses
-                        (nrepl/message session 
+                        (nrepl/message session
                           {:op "load-file"
                            :file "(ns foo) (def x 5)"
                            :file-path "/path/to/source.clj"
