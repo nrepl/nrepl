8428621a5d3a576aa46e27788d2609e4240d90d3
Bozhidar Batsov <bozhidar@batsov.com>
2018-05-20 08:58:00 +0300

    Add better license information

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject nrepl "0.3.1"
   :description "nREPL is a Clojure *n*etwork REPL."
   :url "https://github.com/nrepl/nREPL"
-  :license "EPL"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :min-lein-version "2.6.1"
   :source-paths ["src/clojure"]
   :java-source-paths ["src/java"]
