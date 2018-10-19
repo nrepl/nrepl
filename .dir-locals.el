aca5fbe83807d851226d488c47848815c25cea3c
Bozhidar Batsov <bozhidar@batsov.com>
2018-10-20 00:42:18 +0200

    Add some default project settings for Emacs users
    This ensure that everyone is using the same indentation style
    and no tabs.

---
+++ .dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((clojure-mode
+  (clojure-indent-style . :always-align)
+  (indent-tabs-mode . nil)
+  (fill-column . 80)))
