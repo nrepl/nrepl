9c40403a299c61cb318b183c1600be2ca364619e
Tijs Mallaerts <mallt@users.noreply.github.com>
2018-11-14 18:22:06 +0100

    Support evaluation interrupt in interactive repl session (#81)

--- src/clojure/nrepl/cmdline.clj
+++ src/clojure/nrepl/cmdline.clj
@@ -13,6 +13,18 @@
    [nrepl.transport :as transport]
    [nrepl.version :as version]))
 
+(defmacro ^{:author "Colin Jones"} set-signal-handler!
+  [signal f]
+  (if (try (Class/forName "sun.misc.Signal")
+           (catch Throwable e))
+    `(try
+       (sun.misc.Signal/handle
+        (sun.misc.Signal. ~signal)
+        (proxy [sun.misc.SignalHandler] []
+          (handle [signal#] (~f signal#))))
+       (catch Throwable e#))
+    `(println "Unable to set signal handlers.")))
+
 (def colored-output
   {:err #(binding [*out* *err*]
            (print "\033[31m")
@@ -25,6 +37,9 @@
             (print x)
             (println "\033[m")
             (flush))})
+
+(def running-repl (atom {:transport nil
+                         :client nil}))
 
 (defn- done?
   [input]
@@ -42,9 +57,13 @@
    (let [transport (nrepl/connect :host host :port port)
          client (nrepl/client-session (nrepl/client transport Long/MAX_VALUE))
          ns (atom "user")]
+     (swap! running-repl assoc :transport transport)
+     (swap! running-repl assoc :client client)
      (println (format "nREPL %s" (:version-string version/version)))
      (println (str "Clojure " (clojure-version)))
      (println (System/getProperty "java.vm.name") (System/getProperty "java.runtime.version"))
+     (println (str "Interrupt: Control+C"))
+     (println (str "Exit:      Control+D or (exit) or (quit)"))
      (loop []
        (prompt @ns)
        (flush)
@@ -139,6 +158,16 @@
       (into [] resolve-mw-xf x)
       [var])))
 
+(defn- handle-interrupt
+  [signal]
+  (let [transport (:transport @running-repl)
+        client (:client @running-repl)]
+    (if (and transport client)
+      (doseq [res (nrepl/message client {:op :interrupt})]
+        (when (= ["done" "session-idle"] (:status res))
+          (System/exit 0)))
+      (System/exit 0))))
+
 (def ^:private mw-xf
   (comp (map symbol)
         resolve-mw-xf
@@ -188,6 +217,7 @@
 
 (defn -main
   [& args]
+  (set-signal-handler! "INT" handle-interrupt)
   (let [[options _args] (split-args (expand-shorthands args))
         options (keywordize-options options)
         options (merge config/config options)]
