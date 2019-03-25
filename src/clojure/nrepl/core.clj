51455fb80c6a3f864740f10954da9771ec1cfa4f
Shen Tian <biozinc@gmail.com>
2019-03-25 19:24:01 +0200

    Reverted change to delimited-transport-seq. Tests still pass

--- src/clojure/nrepl/core.clj
+++ src/clojure/nrepl/core.clj
@@ -3,7 +3,7 @@
   {:author "Chas Emerick"}
   (:require
    clojure.set
-   [nrepl.misc :refer [uuid keyworded-set]]
+   [nrepl.misc :refer [uuid]]
    [nrepl.transport :as transport]
    [nrepl.version :as version])
   (:import
@@ -66,9 +66,7 @@
 (defn- delimited-transport-seq
   [client termination-statuses delimited-slots]
   (with-meta
-    (comp (partial take-until (comp #(seq (clojure.set/intersection
-                                           (keyworded-set %)
-                                           (keyworded-set termination-statuses)))
+    (comp (partial take-until (comp #(seq (clojure.set/intersection % termination-statuses))
                                     set
                                     :status))
           (let [keys (keys delimited-slots)]
