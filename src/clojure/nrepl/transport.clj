84ed56d9bd68cebbaf9f01b0aca6ca4ea0fc3f25
Shen Tian <biozinc@gmail.com>
2019-03-26 18:38:15 +0200

    Adding hte missing defmethod for edn transport

--- src/clojure/nrepl/transport.clj
+++ src/clojure/nrepl/transport.clj
@@ -225,6 +225,8 @@
 
 (defmethod uri-scheme #'tty [_] "telnet")
 
+(defmethod uri-scheme #'edn [_] "nrepl+edn")
+
 (defmethod uri-scheme :default
   [transport]
   (printf "WARNING: No uri scheme associated with transport %s\n" transport)
