f242f868d0c9c78b295b40683ff5817796ca00b1
Bozhidar Batsov <bozhidar@batsov.com>
2020-04-23 10:40:41 +0300

    Clean up a bit the completion logic

--- src/clojure/nrepl/completion.clj
+++ src/clojure/nrepl/completion.clj
@@ -15,29 +15,30 @@
 ;; The results follow compliment's format, as it's richer and gives more useful
 ;; data to clients.
 
+;;; Utility functions
 (defn namespaces
   "Returns a list of potential namespace completions for a given namespace"
   [ns]
-  (map #(hash-map :candidate (name %) :type :namespace) (concat (map ns-name (all-ns)) (keys (ns-aliases ns)))))
+  (concat (map ns-name (all-ns)) (keys (ns-aliases ns))))
 
 (defn ns-public-vars
   "Returns a list of potential public var name completions for a given namespace"
   [ns]
-  (map #(hash-map :candidate (name %) :type :var) (keys (ns-publics ns))))
+  (keys (ns-publics ns)))
 
 (defn ns-vars
   "Returns a list of all potential var name completions for a given namespace"
   [ns]
   (for [[sym val] (ns-map ns) :when (var? val)]
-    {:candidate (name sym) :type :var}))
+    sym))
 
 (defn ns-classes
   "Returns a list of potential class name completions for a given namespace"
   [ns]
-  (map #(hash-map :candidate (name %) :type :class) (keys (ns-imports ns))))
+  (keys (ns-imports ns)))
 
 (def special-forms
-  (map #(hash-map :candidate (name %) :type :special-form :ns "clojure.core") '[def if do let quote var fn loop recur throw try monitor-enter monitor-exit dot new set!]))
+  '[def if do let quote var fn loop recur throw try monitor-enter monitor-exit dot new set!])
 
 (defn- static? [#^java.lang.reflect.Member member]
   (java.lang.reflect.Modifier/isStatic (.getModifiers member)))
@@ -46,13 +47,13 @@
   "Returns a list of potential java method name completions for a given namespace"
   [ns]
   (for [class (vals (ns-imports ns)) method (.getMethods ^Class class) :when (static? method)]
-    {:candidate (str "." (.getName ^Member method)) :type :method}))
+    (str "." (.getName ^Member method))))
 
 (defn static-members
   "Returns a list of potential static members for a given class"
   [^Class class]
   (for [member (concat (.getMethods class) (.getDeclaredFields class)) :when (static? member)]
-    {:candidate (.getName ^Member member) :type :static-method}))
+    (.getName ^Member member)))
 
 (defn path-files [^String path]
   (cond (.endsWith path "/*")
@@ -82,13 +83,13 @@
   (future
     (doall
      (for [file classfiles :when (re-find #"^[^\$]+\.class" file)]
-       {:candidate (classname file) :type :class}))))
+       (classname file)))))
 
 (def nested-classes
   (future
     (doall
      (for [file classfiles :when (re-find #"^[^\$]+(\$[^\d]\w*)+\.class" file)]
-       {:candidate (classname file) :type :class}))))
+       (classname file)))))
 
 (defn resolve-class [sym]
   (try (let [val (resolve sym)]
@@ -98,45 +99,82 @@
                      (class (clojure.main/repl-exception e)))
            (throw e)))))
 
-(defmulti potential-completions
-  (fn [^String prefix ns]
-    (cond
-      (.startsWith prefix ".") :method
-      (.contains prefix "/")  :scoped
-      (.contains prefix ".")  :class
-      :else                   :var)))
+;;; Candidates
 
-(defmethod potential-completions :method
-  [^String prefix ns]
-  (ns-java-methods ns))
+(defn annotate-var [var]
+  {:candidate (name var) :type :var})
 
-(defmethod potential-completions :scoped
-  [^String prefix ns]
+(defn annotate-class
+  [cname]
+  {:candidate cname :type :class})
+
+(def special-form-candidates
+  (map #(hash-map :candidate (name %) :type :special-form :ns "clojure.core") special-forms))
+
+(defn ns-candidates
+  [ns]
+  (map #(hash-map :candidate (name %) :type :namespace) (namespaces ns)))
+
+(defn ns-var-candidates
+  [ns]
+  (map annotate-var (ns-vars ns)))
+
+(defn ns-public-var-candidates
+  [ns]
+  (map annotate-var (ns-public-vars ns)))
+
+(defn ns-class-candidates
+  [ns]
+  (map #(hash-map :candidate (name %) :type :class) (ns-classes ns)))
+
+(defn ns-java-method-candidates
+  [ns]
+  (for [method (ns-java-methods ns)]
+    {:candidates method :type :method}))
+
+(defn static-member-candidates
+  [class]
+  (for [name (static-members class)]
+    {:candidate name :type :static-method}))
+
+(defn scoped-candidates
+  [prefix ns]
   (when-let [prefix-scope (first (.split prefix "/"))]
     (let [scope (symbol prefix-scope)]
       (map #(update % :candidate (fn [c] (str scope "/" c)))
            (if-let [class (resolve-class scope)]
-             (static-members class)
+             (static-member-candidates class)
              (when-let [ns (or (find-ns scope) (scope (ns-aliases ns)))]
-               (ns-public-vars ns)))))))
+               (ns-public-var-candidates ns)))))))
 
-(defmethod potential-completions :class
-  [^String prefix ns]
-  (concat (namespaces ns)
-          (if (.contains prefix "$")
-            @nested-classes
-            @top-level-classes)))
-
-(defmethod potential-completions :var
+(defn class-candidates
   [prefix ns]
-  (concat special-forms
-          (namespaces ns)
-          (ns-vars ns)
-          (ns-classes ns)))
+  (map annotate-class
+       (if (.contains prefix "$")
+         @nested-classes
+         @top-level-classes)))
+
+(defn generic-candidates
+  [ns]
+  (concat special-form-candidates
+          (ns-candidates ns)
+          (ns-var-candidates ns)
+          (ns-class-candidates ns)))
+
+(defn completion-candidates
+  [prefix ns]
+  (cond
+    (.startsWith prefix ".") (ns-java-method-candidates ns)
+    (.contains prefix "/")  (scoped-candidates prefix ns)
+    (.contains prefix ".")  (concat (ns-candidates ns) (class-candidates prefix ns))
+    :else                   (generic-candidates ns)))
 
 (defn completions
   "Return a sequence of matching completion candidates given a prefix string and an optional current namespace."
   ([prefix]
    (completions prefix *ns*))
-  ([^String prefix ns]
-   (sort-by :candidate (filter #(.startsWith (:candidate %) prefix) (potential-completions prefix ns)))))
+  ([prefix ns]
+   (completions prefix ns nil))
+  ([^String prefix ns _options]
+   (let [candidates (completion-candidates prefix ns)]
+     (sort-by :candidate (filter #(.startsWith (:candidate %) prefix) candidates)))))
