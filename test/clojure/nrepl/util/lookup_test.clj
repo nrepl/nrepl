150f9be05affaf6a18107b00284dae86b1650643
Bozhidar Batsov <bozhidar@batsov.com>
2020-06-06 12:27:06 +0300

    Revert "Add handling of aliased symbols in the lookup middleware"
    This reverts commit cb97ec90670d76ed721a1d8a98607433a74d6d5b.

--- test/clojure/nrepl/util/lookup_test.clj
+++ test/clojure/nrepl/util/lookup_test.clj
@@ -8,10 +8,10 @@
     (is (not-empty (lookup 'clojure.core 'if))))
 
   (testing "fully qualified sym lookup"
-    (is (not-empty (lookup 'clojure.core 'clojure.core/map))))
+    (is (not-empty (lookup 'whatever 'clojure.core/map))))
 
-  (testing "aliased sym lookup"
-    (is (not-empty (lookup 'nrepl.util.lookup 'str/upper-case))))
+  #_(testing "aliased sym lookup"
+      (is (not-empty (lookup 'whatever 'str/upper-case))))
 
   (testing "non-qualified lookup"
     (is (not-empty (lookup 'clojure.core 'map))))
