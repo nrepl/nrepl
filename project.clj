45bd5b4c8ef99161b28ba388d0bd479bc1579b9b
Bozhidar Batsov <bozhidar@batsov.com>
2020-06-25 11:18:12 +0300

    Release 0.8.0-alpha5

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nrepl "0.8.0-alpha4"
+(defproject nrepl "0.8.0-alpha5"
   :description "nREPL is a Clojure *n*etwork REPL."
   :url "https://nrepl.org"
   :license {:name "Eclipse Public License"
