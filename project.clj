f7d489f4027671b78713a033b97aae84b57d5858
Bozhidar Batsov <bozhidar@batsov.com>
2021-07-29 08:19:09 +0300

    Release 0.9.0-beta1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject nrepl "0.9.0-SNAPSHOT"
+(defproject nrepl "0.9.0-beta1"
   :description "nREPL is a Clojure *n*etwork REPL."
   :url "https://nrepl.org"
   :license {:name "Eclipse Public License"
