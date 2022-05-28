ff86a69e3601f222f7962763ceb3955326729db8
Jason Darby <jasonkdarby@gmail.com>
2022-05-28 19:33:00 -0400

    :read-cond :allow for reading during tty transport

--- test/clojure/nrepl/transport_test.clj
+++ test/clojure/nrepl/transport_test.clj
@@ -9,3 +9,13 @@
       (is (thrown? IllegalArgumentException
                    (#'sut/safe-write-bencode out {"obj" (Object.)})))
       (is (empty? (.toByteArray out))))))
+
+(deftest tty-read-conditional-test
+  (testing "tty-read-msg is configured to read conditionals"
+    (let [r (-> "(try nil (catch #?(:clj Throwable :cljr Exception) e nil))"
+                (java.io.StringReader.)
+                (java.io.PushbackReader.))
+          cns (atom "user")
+          session-id (atom nil)]
+      (is (= ['(try nil (catch Throwable e nil))]
+             (:code (sut/tty-read-msg r cns session-id)))))))
