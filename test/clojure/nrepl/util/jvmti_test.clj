2cf2719a9f869b60343bf51e7caa0b1e4c704ffe
Oleksandr Yakushev <alex@bytopia.org>
2024-05-28 10:50:56 +0300

    [tests] Use :min-java-version instead of manually checking for java-version

--- test/clojure/nrepl/util/jvmti_test.clj
+++ test/clojure/nrepl/util/jvmti_test.clj
@@ -2,13 +2,12 @@
   (:require [nrepl.misc :as misc]
             [clojure.test :refer :all]))
 
-(when (> misc/java-version 8)
-  (deftest stop-thread-test
-    (let [vol (volatile! 0)
-          t (doto (Thread. #(while (vswap! vol inc))) .start)]
-      ((misc/requiring-resolve 'nrepl.util.jvmti/stop-thread) t)
+(deftest ^{:min-java-version "11.0"} stop-thread-test
+  (let [vol (volatile! 0)
+        t (doto (Thread. #(while (vswap! vol inc))) .start)]
+    ((misc/requiring-resolve 'nrepl.util.jvmti/stop-thread) t)
+    (Thread/sleep 1000)
+    (let [v @vol]
       (Thread/sleep 1000)
-      (let [v @vol]
-        (Thread/sleep 1000)
-        (is (= v @vol))
-        (is (= "TERMINATED" (str (.getState t))))))))
+      (is (= v @vol))
+      (is (= "TERMINATED" (str (.getState t)))))))
