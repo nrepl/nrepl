4cac422863bf71ea957482865252e1dbf7effcf7
Oleksandr Yakushev <alex@bytopia.org>
2025-09-20 12:58:32 +0300

    [load-file] Reimplement load-file again

--- src/clojure/nrepl/middleware/load_file.clj
+++ src/clojure/nrepl/middleware/load_file.clj
@@ -1,67 +1,66 @@
 (ns nrepl.middleware.load-file
   {:author "Chas Emerick"}
-  (:require
-   [nrepl.middleware :as middleware :refer [set-descriptor!]]
-   [nrepl.middleware.caught :as caught]
-   [nrepl.middleware.interruptible-eval :as eval]
-   [nrepl.middleware.print :as print])
-  (:import clojure.lang.Compiler
-           java.io.StringReader
-           nrepl.transport.Transport))
+  (:require [nrepl.middleware :as middleware :refer [set-descriptor!]]
+            [nrepl.middleware.caught :as caught]
+            [nrepl.middleware.interruptible-eval :as eval]
+            [nrepl.middleware.print :as print]
+            [nrepl.misc :refer [resolve-in-session]])
+  (:import (clojure.lang Compiler)
+           (nrepl.transport Transport)))
 
-;; Be vary of JVM method size limit (can't eval an expression larger than 64KB).
-;; http://groups.google.com/group/clojure/browse_thread/thread/f54044da06b9939f
+;; In this middleware, we implement loading whole the file by nREPL from start
+;; to finish. This used to be implemented by calling out to `Compiler/load`.
+;; However, `Compiler/load` doesn't allow to customize the evaluation function
+;; it uses, so now the middleware is implemented completely by delegating to
+;; `interruptible-eval` middleware to do the reading and evaling. All that's
+;; left to do here is to make sure that the behavior is still consistent with
+;; the old approach.
 
-(defn ^{:dynamic true, :deprecated "1.4.0"}
-  load-file-code
-  "You should use the approach implemented in `load-file-eval-msg` instead.
+(defn- per-file-bindings [msg]
+  {Compiler/METHOD nil
+   Compiler/LOCAL_ENV nil
+   Compiler/LOOP_LOCALS nil
+   Compiler/NEXT_LOCAL_NUM 0
+   ;; We don't set LINE_BEFORE, COLUMN_BEFORE, LINE_AFTER, COLUMN_AFTER because
+   ;; it looks like it doesn't change much for our usecase. But this is still to
+   ;; be confirmed.
+   #'*read-eval* true
+   ;; This function runs in "server context" (not yet in session context), so
+   ;; make sure to resolve dynvar variables from session.
+   #'*ns* (resolve-in-session msg *ns*)
+   #'*unchecked-math* (resolve-in-session msg *unchecked-math*)
+   #'*warn-on-reflection* (resolve-in-session msg *warn-on-reflection*)
+   #'*data-readers* (resolve-in-session msg *data-readers*)})
 
-   Given the contents of a file, its _source-path-relative_ path,
-   and its filename, returns a string of code containing a single
-   expression that, when evaluated, will load those contents with
-   appropriate filename references and line numbers in metadata, etc.
-
-   Note that because a single expression is produced, very large
-   file loads will fail due to the JVM method size limitation.
-   In such cases, see `load-large-file-code'`."
-  [file file-path file-name]
-  (apply format
-         "(clojure.lang.Compiler/load (java.io.StringReader. %s) %s %s)"
-         (map (fn [item]
-                (binding [*print-length* nil
-                          *print-level* nil]
-                  (pr-str item)))
-              [file file-path file-name])))
-
-(defn- load-file-contents [[file file-path file-name]]
-  (Compiler/load (StringReader. file) file-path file-name))
-
-#_{:clj-kondo/ignore [:deprecated-var]}
 (defn- load-file-eval-msg
-  [{:keys [file file-path file-name ^Transport transport] :as msg}]
-  (let [transport (reify Transport
+  [{:keys [file file-path ^Transport transport] :as msg}]
+  (let [last-result (volatile! nil)
+        error-encountered (volatile! false)
+        wrapped-t (reify Transport
                     (recv [_this] (.recv transport))
                     (recv [_this timeout] (.recv transport timeout))
-                    (send [this resp]
-                      ;; *ns* is always 'user' after loading a file, so
-                      ;; *remove it to avoid confusing tools that assume any
-                      ;; *:ns always reports *ns*
-                      (.send transport (dissoc resp :ns))
-                      this))
-        eval-msg (-> (dissoc msg :file :file-name :file-path)
-                     (assoc :op "eval", :transport transport))]
-    (if (thread-bound? #'load-file-code)
-      ;; This approach is left for backward compatibility
-      (assoc eval-msg :code (load-file-code file file-path file-name))
-      ;; This is a bit of hack to make `eval` mw just call the code we want.
-      ;; `:code` receives a tuple of the required arguments, wrapped in another
-      ;; vector on top (so `eval` doesn't try to read anything and just use it
-      ;; as input to `:eval` function), and we also provide a custom `:eval` to
-      ;; delegate loading to `Compiler/load`. This approach sidesteps the method
-      ;; size issue.
-      (assoc eval-msg
-             :code [[file file-path file-name]]
-             :eval `load-file-contents))))
+                    (send [_this resp]
+                      ;; Whenever the result is returned to the client, remember
+                      ;; and don't send it, since `load-file` is supposed to
+                      ;; only "return" the result of the last form in the file.
+                      (if (contains? resp :value)
+                        (vreset! last-result resp)
+                        (do (when (:ex resp)
+                              ;; If an error is thrown, don't send any
+                              ;; successful result to the client.
+                              (vreset! error-encountered true))
+                            ;; This is our signal to send the last result back
+                            ;; to the client before sending `done`. Dissoc `:ns`
+                            ;; to avoid confusing tools which assume any
+                            ;; :ns always means *ns*.
+                            (when (and (contains? (:status resp) :done)
+                                       (not @error-encountered))
+                              (.send transport (dissoc @last-result :ns)))
+                            (.send transport resp)))))]
+    (-> (dissoc msg :file-path)
+        (assoc :op "eval", :code file, :transport wrapped-t, :file file-path
+               ::eval/stop-on-error true
+               ::eval/bindings (per-file-bindings msg)))))
 
 (defn wrap-load-file
   "Middleware that evaluates a file's contents, as per load-file,
