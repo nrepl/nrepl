a454103ad9f998294bdc433e59da05740231a988
Oleksandr Yakushev <alex@bytopia.org>
2026-03-31 17:50:25 +0300

    [out] Fix tearing of multi-byte characters during output flushing

--- src/java/nrepl/out/CallbackBufferedOutputStream.java
+++ src/java/nrepl/out/CallbackBufferedOutputStream.java
@@ -19,16 +19,37 @@ public class CallbackBufferedOutputStream extends OutputStream {
     public CallbackBufferedOutputStream(IFn callback, int bufferSize) {
         this.callback = callback;
         this.bufferSize = bufferSize;
-        this.charset = Charset.defaultCharset();
+        this.charset = Charset.forName("UTF-8");
         this.buffer = new ByteArrayOutputStream();
+    }
+
+    private static boolean isSingleByteChar(byte b) {
+        return (b & 0b10000000) == 0;
+    }
+
+    private static boolean isCharacterStartingByte(byte b) {
+        return isSingleByteChar(b) ||
+            (b & 0b11000000) == 0b11000000; // start of multi-byte UTF-8 character
+    }
+
+    private static int clamp(int value, int min, int max) {
+        return Math.min(Math.max(value, min), max);
     }
 
     @Override
     public void write(int b) throws IOException {
         synchronized (bufferLock) {
-            buffer.write(b);
-            if (b == '\n' || buffer.size() >= bufferSize)
-                maybeFlush(false);
+            if (isSingleByteChar((byte)b)) {
+                buffer.write(b);
+                if (b == '\n' || buffer.size() >= bufferSize)
+                    maybeFlush(false);
+            } else {
+                // If we are behind flush because of multi-byte characters,
+                // flush first and then append to fresh buffer.
+                if (buffer.size() >= bufferSize && isCharacterStartingByte((byte)b))
+                    maybeFlush(false);
+                buffer.write(b);
+            }
         }
     }
 
@@ -37,9 +58,26 @@ public class CallbackBufferedOutputStream extends OutputStream {
         synchronized (bufferLock) {
             int end = off + len;
             while (off < end) {
-                int writeLen = Math.min(len, bufferSize - buffer.size());
+                // Make sure to clamp to at least 1 character because
+                // buffer.size() might run above bufferSize if multi-byte
+                // characters are involved.
+                int writeLen = clamp(bufferSize - buffer.size(), 1, len);
+                boolean canFlush = false;
+                if (isSingleByteChar(b[off+writeLen-1])) {
+                    canFlush = true;
+                } else {
+                    // Scan until the next character-starting byte to avoid
+                    // character tearing.
+                    while (writeLen+1 < len) {
+                        if (isCharacterStartingByte(b[off+writeLen])) {
+                            canFlush = true;
+                            break;
+                        }
+                        writeLen++;
+                    }
+                }
                 buffer.write(b, off, writeLen);
-                maybeFlush(false);
+                if (canFlush) maybeFlush(false);
                 off += writeLen;
                 len -= writeLen;
             }
@@ -71,7 +109,7 @@ public class CallbackBufferedOutputStream extends OutputStream {
         String content = buffer.toString(charset.name());
         int length = content.length();
         int lastNewlineIndex = content.lastIndexOf('\n');
-        int lengthToFlush = (force || size == bufferSize) ? length : lastNewlineIndex + 1;
+        int lengthToFlush = (force || size >= bufferSize) ? length : lastNewlineIndex + 1;
         if (lengthToFlush > 0) {
             String flushContent = content.substring(0, lengthToFlush);
             callback.invoke(flushContent);
